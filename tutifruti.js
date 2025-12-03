6dfe5a4e73c5857530fe545e3361b7016e2deb65
Rodrigo Fernandez <rdr.rkh+github2@gmail.com>
2025-12-02 21:47:30 -0300

    Primer vuelta del desarrollo.

--- tutifruti.js
+++ tutifruti.js
@@ -8,9 +8,39 @@ function obtenerElementoRandom(elementos) {
     return elementos[indiceRandom];
 }
 
-function prueba(){
-    //const letras = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','Ñ','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-    const letras = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','R','S','T','U','V','Z'];
+function getLetrasRestantes(letras, letrasUsadas){
+    const letrasUsadasAux = new Set(letrasUsadas);
+    const letrasAux = new Set(letras);
 
-    console.log(obtenerElementoRandom(letras));
+    let letrasRestantes = [];
+
+    letrasAux.forEach((letra) => {
+        if(!letrasUsadasAux.has(letra)){
+            letrasRestantes.push(letra);
+        }
+    });
+
+    return letrasRestantes;
 }
+
+function getLetra(){
+    //const letras = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','ñ','o','p','q','r','s','t','u','v','w','x','y','z'];
+    const letras = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','r','s','t','u','v','z'];
+    
+    const letrasUsadas = document.getElementById("inputPalabra").value.split("");
+    console.log(letrasUsadas);
+
+    const letrasRestantes = getLetrasRestantes(letras, letrasUsadas);
+    let letraElegida = obtenerElementoRandom(letrasRestantes);
+    console.log(letraElegida);
+    console.log(letrasRestantes);
+
+    document.getElementById("letra").src = `./img/${letraElegida}.svg`;
+
+    document.getElementById("inputPalabra").value = document.getElementById("inputPalabra").value + letraElegida;
+
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("inputPalabra").value = "";
+});
